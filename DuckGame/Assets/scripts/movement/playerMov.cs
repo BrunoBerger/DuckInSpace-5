03c24cf2923031a2c5f90692ef51b494519987cd
BrunoBerger <bruno.berger@stud.hshl.de>
2020-09-07 19:30:20 +0200

    basic jump test

--- DuckGame/Assets/scripts/movement/playerMov.cs
+++ DuckGame/Assets/scripts/movement/playerMov.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class playerMov : MonoBehaviour
 {
+    Rigidbody2D rb;
     Vector2 userInput;
-    float movSpeed = 0.2f;
+
+    public bool isGrounded;
+    public float movSpeed = 0.15f;
+    public float jumpStr = 10f;
+    // float grav = 0.05f;
+
     // Start is called before the first frame update
     void Start()
     {
+      rb = transform.GetComponent<Rigidbody2D>();
       print("Duck inited");
       print(transform.position);
     }
@@ -20,17 +27,31 @@ public class playerMov : MonoBehaviour
     }
     void FixedUpdate()
     {
+      // Horizontal Movement
       userInput.Set(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
       Debug.DrawRay(transform.position, userInput*2, Color.red);
-
       transform.Translate(userInput * movSpeed);
+
+
+      if (isGrounded == false)
+      {
+      //   transform.Translate(-Vector2.up * grav);
+      }
+      else
+      {
+        // Jumping
+        if (Input.GetButton("Jump"))
+        {
+          rb.velocity = Vector2.up * jumpStr;
+        }
+      }
     }
 
 
     private void OnCollisionEnter2D(Collision2D collidor){
-      print("On Ground");
+      isGrounded = true;
     }
     private void OnCollisionExit2D(Collision2D collidor){
-      print("left ground");
+      isGrounded = false;
     }
 }
