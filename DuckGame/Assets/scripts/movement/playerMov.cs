93370dc0dd31b61c9a1c2a36d358c2bfed3e2f91
BrunoBerger <bruno.berger@stud.hshl.de>
2020-09-07 18:07:39 +0200

    first 2D movement test

---
+++ DuckGame/Assets/scripts/movement/playerMov.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMov : MonoBehaviour
+{
+    Vector2 userInput;
+    float movSpeed = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      print("Duck inited");
+      print(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+      userInput.Set(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+      Debug.DrawRay(transform.position, userInput*2, Color.red);
+
+      transform.Translate(userInput * movSpeed);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collidor){
+      print("On Ground");
+    }
+    private void OnCollisionExit2D(Collision2D collidor){
+      print("left ground");
+    }
+}
