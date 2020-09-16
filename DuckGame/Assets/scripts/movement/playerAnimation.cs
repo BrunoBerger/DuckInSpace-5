8a45faa14c011a71ffb59989e8eb4c60e964c4bd
BrunoBerger <bruno.berger@stud.hshl.de>
2020-09-16 22:59:35 +0200

    first anims implemented

--- DuckGame/Assets/scripts/movement/playerAnimation.cs
+++ DuckGame/Assets/scripts/movement/playerAnimation.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class playerAnimation : MonoBehaviour
 {
     public Animator animator;
+    private playerMov parent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        parent = this.transform.parent.GetComponent<playerMov>();
     }
 
     // Update is called once per frame
+    // get current Player state and adjust animator state
     void Update()
     {
-        animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
-        print(animator.GetFloat("horizontal"));
+        // check if on ground
+        if (parent.isGrounded){
+            animator.SetBool("isGrounded", true);
+        }else{
+            animator.SetBool("isGrounded", false);
+        }
+
+        //check if jumping
+        if (Input.GetButton("Jump")){
+            animator.SetBool("jumping", true);
+        }else{
+            animator.SetBool("jumping", false);
+        }
     }
 }
