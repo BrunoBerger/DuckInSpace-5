649b211bc6cbc66066b579dce8c8fdaef2e60ef7
BrunoBerger <bruno.berger@stud.hshl.de>
2020-09-22 19:49:46 +0200

    created and switched to tile-based ground

--- DuckGame/Assets/scripts/cameraFollow.cs
+++ DuckGame/Assets/scripts/cameraFollow.cs
@@ -6,12 +6,12 @@ public class cameraFollow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
-    [Range(0, 0.1f)]public float followSpeed;
+    [Range(0, 0.5f)]public float followSpeed;
 
     private Vector3 desiredPosition;
     private Vector3 smoothedPosition;
 
-    private void FixedUpdate() 
+    private void FixedUpdate()
     {
         desiredPosition = target.position + offset;
         smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, followSpeed);
